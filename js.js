f5ccbba9038fe257f3275b696f3bd81c2f3215f3
Marouane <marouaneben2016@gmail.com>
2021-04-12 19:08:53 +0000

    .

---
+++ js.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(function(){
+	jQuery('.skillbar').each(function(){
+		jQuery(this).find('.skillbar-bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},6000);
+	});
+});
+
+jQuery('.Count').each(function () {
+  var $this = $(this);
+  jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
+    duration: 6000,
+    easing: 'swing',
+    step: function () {
+      $this.text(Math.ceil(this.Counter));
+    }
+  });
+});
